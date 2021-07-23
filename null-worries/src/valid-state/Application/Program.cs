c0a5be17230f417351f577c26d4e6c17e3ae4c6e
Alistair Grant <al.grant@bigpond.com>
2021-07-24 11:56:17 +1200

    Add valid state sample code

---
+++ null-worries/src/valid-state/Application/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using ValidState;
+
+// run-time error
+Book a = new(null);
+
+Book b = new("Screens");
+
+Author author = new();
+var hasBooks = author.Books.Any();
---
+++ null-worries/src/valid-state/ClassLibrary/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ValidState
+{
+    public class Book
+    {
+        public Book(string title)
+        {
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+        }
+
+        public string Title { get; }
+    }
+}
