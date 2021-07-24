b5d47f03a89d3b918425c64fc328301f47856720
Alistair Grant <al.grant@bigpond.com>
2021-07-24 12:06:49 +1200

    Add nullable references sample code

---
+++ null-worries/src/nullable-references/Application/Program.cs
@@ -0,0 +1,10 @@
+﻿var obj = new object();
+var text = obj.ToString();
+
+// CS8602 Dereference of a possibly null reference
+var n = text.Length;
+
+if (text != null)
+{
+    var m = text.Length;
+}
