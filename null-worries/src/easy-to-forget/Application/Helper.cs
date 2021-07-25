7a9d327853f4552b25710902d8b17886332b90b0
Alistair Grant <al.grant@bigpond.com>
2021-07-25 12:43:29 +1200

    Add easy-to-forget sample code

---
+++ null-worries/src/easy-to-forget/Application/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyToForget
+{
+    public static class Helper
+    {
+        private static readonly Random _random = new Random();
+
+        public static object GetObject() =>
+            _random.Next() % 2 == 0 ? new() : null;
+    }
+}
