5788be90118d06e7d522d3427ca9d89fb61551bb
Alistair Grant <alistair.grant@ptml.com>
2021-08-02 14:26:33 +1200

    Add sample for absence of a value

---
+++ null-worries/src/02-absence-of-value/Application/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NullWorries
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Jane Citizen
+            Person a = new("Jane", "Citizen");
+
+            // unknown value: John ?
+            Person b = new("John", null);
+
+            // unnecessary value: Lorde
+            Person c = new("Lorde", null);
+        }
+    }
+}
---
+++ null-worries/src/02-absence-of-value/ClassLibrary/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace NullWorries
+{
+    public class Person
+    {
+        public Person(string givenName, string familyName)
+        {
+            GivenName = givenName;
+            FamilyName = familyName;
+        }
+
+        public string GivenName { get; set; }
+
+        public string FamilyName { get; set; }
+    }
+}
