3ee2eb940b42d8c778b60c322a4238e072f09ed2
Alistair Grant <alistair.grant@ptml.com>
2021-08-02 16:32:05 +1200

    Add sample for unspecified value

---
+++ null-worries/src/03-unspecified-value/Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NullWorries
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // valid date of birth
+            Person a = new("Jane", "Citizen", DateTime.Parse("2000-12-25"));
+
+            // born 01 April 3000
+            Person b = new("Jane", "Citizen", DateTime.Parse("3000-04-01"));
+
+            // choice to omit date of birth
+            Person c = new("Jane", "Citizen", null);
+        }
+    }
+}
