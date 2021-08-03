feb9b6ebb8271520e82c6aba91f81b446b510c04
Alistair Grant <alistair.grant@ptml.com>
2021-08-03 14:51:50 +1200

    Update presentation slides

--- null-worries/src/03-unspecified-value/Application/Program.cs
+++ null-worries/src/03-unspecified-value/Application/Program.cs
@@ -7,14 +7,11 @@ namespace NullWorries
     {
         private static void Main()
         {
-            // valid date of birth
-            Person a = new("Jane", "Citizen", DateTime.Parse("2000-12-25"));
-
             // born 01 April 3000
-            Person b = new("Jane", "Citizen", DateTime.Parse("3000-04-01"));
+            Person a = new("Jane", "Citizen", DateTime.Parse("3000-04-01"));
 
             // choice to omit date of birth
-            Person c = new("Jane", "Citizen", null);
+            Person b = new("Jane", "Citizen", null);
         }
     }
 }
